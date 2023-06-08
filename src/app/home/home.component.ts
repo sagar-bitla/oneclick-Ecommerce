4c7b116ac53c78eda53822b6b696cf54d5373c27
unknown <Administrator@Mitesh-Khandelwal.corp.dealmoney.in>
2023-06-08 17:24:01 +0530

    whatsapp changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,20 +10,28 @@ import { ProductService } from '../services/product.service';
 export class HomeComponent implements OnInit {
   popularProducts: undefined | Product[]
   trendyProducts: undefined | Product[]
+  phoneNumber: any;
 
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.productService.popularProduct().subscribe((res) => {
+    this.productService.popularProduct().subscribe((res:any) => {
       console.log("aAaAA", res)
       this.popularProducts = res
     })
-    this.productService.trendyProducts().subscribe((res) => {
+    this.productService.trendyProducts().subscribe((res:any) => {
       this.trendyProducts = res
     })
+    
   }
 
-
+    redirect(data:any){
+      console.log(data.phone)
+      let name=data.name
+      console.log("naaa",name )
+      let url="https://wa.me/9924279484?text="+"%0aHi Sir,%0a"+"My Name is "+name+"%0aThanking You....!"
+      window.open(url, '_blank');
+    }
 
 }
