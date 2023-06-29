b98dad23294a899edb85e7c4004923eccc18a5f9
sagar-bitla <sagarbitla123@gmail.com>
2023-06-29 22:07:59 +0530

    saaaaa

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,5 +25,52 @@ export class HomeComponent implements OnInit {
   }
 
 
+//   ngOnInit(): void {
+//     this.productService.getObs1().subscribe(
+//       val => console.log(val),
+//       err => console.log(err),
+//       ()=> console.log("obs1 complate"));
+
+//       this.productService.getObs2().subscribe(
+//         val => console.log(val),
+//         err => console.log(err),
+//         ()=> console.log("obs2 complate"));
+
+//       this.subscribe=  this.productService.getObs3().subscribe(
+//           val => console.log(val),
+//           err => console.log(err),
+//           ()=> console.log("obs3 complate"));
+
+//           this.productService.getObs4()
+//           .pipe(filter(val => val%2==0),map(val => val*3))
+//           .subscribe(val => console.log(val));
+
+//         // this.productList=this.productService.getProducts()         //////////////////////////////////////////    imppppppppppppppp
+//         // this.filterProducts=this.productList
+
+//         this.productService.getProducts().subscribe(
+//           data => {
+//             this.productList=data
+//             this.filterProducts=this.productList                              ////   get api through observable 
+//           }
+//         )
+
+//   }
+
+//   productList:Products[]=[]            ////api productlist import in service
+
+
+//   filterProducts=this.productList;
+
+// filterProduct(Rcategory:string):void{
+//   if(Rcategory===Category.ALL)
+//   {
+//     this.filterProducts=this.productList;
+//   }
+//   else{
+//     this.filterProducts=this.productList.filter(pro=>pro.category===Rcategory);
+//   }
+// }
+
 
 }
